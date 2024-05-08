d73b50b0018ef1a78107e1d5672d04b6e3828c78
Alex Babkin <ababkin@Alexs-MacBook-Pro.local>
2024-05-08 11:32:50 +0200

    fixes

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,37 @@
 use lapin::{
     options::*, publisher_confirm::PublisherConfirm, types::FieldTable, BasicProperties, 
-    Consumer, message::Delivery
+    Consumer, ConsumerDelegate
 };
 use anyhow::{*, Result, Error};
+use futures_util::stream::StreamExt;
+use futures_util::Future;
+use std::pin::Pin;
 
+
+// pub use lapin::{
+//     options::{BasicAckOptions, BasicConsumeOptions},
+// };
 pub mod safe_channel;
-use safe_channel::*;
+pub use safe_channel::SafeChannel;
+pub use lapin::{Queue, Channel};
+// pub use lapin::options::*;
+pub use lapin::message::Delivery;
 
+
+pub enum Target {
+    Exchange(String),
+    Queue(String)
+}
+
+impl Target {
+    pub fn mk_queue(name: &str) -> Self {
+        Target::Queue(name.to_string())
+    }
+
+    pub fn mk_exchange(name: &str) -> Self {
+        Target::Exchange(name.to_string())
+    }
+}
 
 pub async fn create_consumer(sc: &SafeChannel, queue: &'static str, consumer_tag: &'static str) -> Result<Consumer, Error> {
     let channel = sc.get().await?;
@@ -19,24 +44,87 @@ pub async fn create_consumer(sc: &SafeChannel, queue: &'static str, consumer_tag
     .await?)
 }
 
-pub async fn publish(sc: &SafeChannel, q: &str, payload: &[u8]) 
-    -> Result<PublisherConfirm, Error> {
-    let channel = sc.get().await?;
-    Ok(channel
-        .basic_publish(
-            "",
-            q,
-            BasicPublishOptions::default(),
-            payload,
-            BasicProperties::default(),
-        )
-        .await?)
-}
-
 pub async fn ack(sc: &SafeChannel, delivery: &Delivery) -> Result<(), Error> {
     let channel = sc.get().await?;
     let delivery_tag = delivery.delivery_tag;
     Ok(channel.basic_ack(
         delivery_tag, BasicAckOptions::default()
     ).await?)
+}
+
+pub async fn enq(sc: &SafeChannel, target: &Target, msg: &[u8]) -> Result<PublisherConfirm, Error> {
+    let channel = sc.get().await?;
+
+    let (exchange_name, queue_name) = match target {
+        Target::Exchange(exchange_name) => (exchange_name.as_str(), ""),
+        Target::Queue(queue_name) => ("", queue_name.as_str())
+    };
+
+    Ok(channel.basic_publish(
+        exchange_name,
+        queue_name,
+        BasicPublishOptions::default(),
+        &msg,
+        BasicProperties::default().with_content_type("application/json".into()).with_delivery_mode(2),
+    ).await?)
+}
+
+pub type AckFn = Box<dyn FnOnce(Delivery) -> Pin<Box<dyn Future<Output = Result<(), Error>> + Send>> + Send>;
+pub type DeliveryHandler = Box<dyn Fn(Delivery, AckFn) -> Pin<Box<dyn Future<Output = Result<(), Error>> + Send>> + Send>;
+
+pub async fn consume_next_message(sc: &SafeChannel, queue_name: &str, handle_delivery: DeliveryHandler) -> Result<(), Error> {
+    let channel = sc.get().await?;
+
+    // Setting prefetch count to 1 to ensure that only one message is processed at a time
+    let options = BasicQosOptions {
+        global: false,      // Apply setting per consumer, not to the entire channel
+    };
+    channel.basic_qos(1, options).await?;
+
+    let mut consumer: Consumer = channel
+        .basic_consume(
+            queue_name,
+            "my_consumer",
+            BasicConsumeOptions::default(),
+            FieldTable::default(),
+        )
+        .await?;
+
+    match consumer.next().await {
+        Some(delivery_result) => {
+            let delivery = delivery_result?;
+
+            let ack: AckFn = Box::new(|dlv| {
+                Box::pin(async move {
+                    Ok(channel.basic_ack(dlv.delivery_tag, lapin::options::BasicAckOptions::default()).await?)
+                })
+            });
+
+            Ok(handle_delivery(delivery, ack).await?)
+        },
+        None => {
+            Err(anyhow!("could not get next message"))
+        }
+    }
+}
+
+
+pub async fn declare(sc: &SafeChannel, name: &str, opts: QueueDeclareOptions) -> Result<Queue, Error> {
+    let chan = sc.get().await?;
+
+    let queue = chan.queue_declare(
+        name,
+        // QueueDeclareOptions::default(),
+        opts,
+        FieldTable::default(),
+    ).await?;
+
+    Ok(queue)
+}
+
+pub fn normal_queue_opts() -> QueueDeclareOptions {
+    QueueDeclareOptions {
+        durable: true,  // Set durable to true to make the queue survive broker restarts
+        ..QueueDeclareOptions::default()
+    }
 }
--- src/safe_channel.rs
+++ src/safe_channel.rs
@@ -13,32 +13,15 @@ impl SafeChannel {
         SafeChannel(Mutex::new(None))
     }
 
-    // pub async fn ensure(&self) {
-    //     loop {
-    //         debug!("Trying to open RabbitMQ channel...");
-    //         match create().await {
-    //             Ok(chan) => {
-    //                 let mut locked = self.0.lock().await;
-    //                 *locked = Some(chan);
-    //                 debug!("RabbitMQ channel is established.");
-    //                 break;
-    //             },
-    //             Err(e) => {
-    //                 error!("Failed to create channel: {:?}", e);
-    //                 tokio::time::sleep(tokio::time::Duration::from_secs(1)).await; // Wait before retrying
-    //             }
-    //         }
-    //     }
-    // }
-
     pub async fn ensure(&self) {
-        let mut locked = self.0.lock().await;
-        if locked.is_none() {
+        loop {
             debug!("Trying to open RabbitMQ channel...");
             match create().await {
                 Ok(chan) => {
+                    let mut locked = self.0.lock().await;
                     *locked = Some(chan);
                     debug!("RabbitMQ channel is established.");
+                    break;
                 },
                 Err(e) => {
                     error!("Failed to create channel: {:?}", e);
@@ -48,21 +31,40 @@ impl SafeChannel {
         }
     }
 
+    // pub async fn ensure(&self) {
+    //     let mut locked = self.0.lock().await;
+    //     if locked.is_none() {
+    //         debug!("Trying to open RabbitMQ channel...");
+    //         match create().await {
+    //             Ok(chan) => {
+    //                 *locked = Some(chan);
+    //                 debug!("RabbitMQ channel is established.");
+    //             },
+    //             Err(e) => {
+    //                 error!("Failed to create channel: {:?}", e);
+    //                 tokio::time::sleep(tokio::time::Duration::from_secs(1)).await; // Wait before retrying
+    //             }
+    //         }
+    //     }
+    // }
+
     pub async fn invalidate(&self) -> () {
         let mut lock = self.0.lock().await;
         *lock = None;
     }
 
     pub async fn get(&self) -> Result<Channel, Error> {
-        {
-            let lock = self.0.lock().await;
-            if let Some(channel) = &*lock {
-                return Ok(channel.clone());  // Clone the channel before returning
+        loop {
+            {
+                let lock = self.0.lock().await;
+                if let Some(channel) = &*lock {
+                    return Ok(channel.clone());  // Clone the channel before returning
+                }
             }
+            warn!("RabbitMQ channel lost, attempting to reconnect...");
+            self.ensure().await;
+            tokio::time::sleep(tokio::time::Duration::from_secs(1)).await; // Wait before retrying
         }
-        warn!("RabbitMQ channel lost, attempting to reconnect...");
-        self.ensure().await;
-        Box::pin(self.get()).await  // Use Box::pin to handle recursion
     }
 
 }
